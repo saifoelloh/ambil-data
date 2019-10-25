d15c1cc815db8e4cfc7151197d69cbd5e59e7c50
saifoelloh <saifoelloh@gmai.com>
2019-10-25 13:16:32 +0700

    method update

--- index.js
+++ index.js
@@ -2,76 +2,78 @@ const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost:27017/ambil-data', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const mahasiswaSchema = new Schema({
-  ipk: String,
-  name: String,
-  ipk: Number,
-  status: String,
-  kuliah: [{ type: Schema.Types.ObjectId, ref: 'kuliah' }],
-});
-
-const makulSchema = new Schema({
-  name: String,
-});
-
-const kuliahSchema = new Schema({
-  makul: { type: Schema.Types.ObjectId, ref: 'makul' },
-  mahasiswa: { type: Schema.Types.ObjectId, ref: 'mahasiswa' },
-  status: String,
-});
-
-const Mahasiswa = mongoose.model('mahasiswa', mahasiswaSchema);
-const Makul = mongoose.model('makul', makulSchema);
-const Kuliah = mongoose.model('kuliah', kuliahSchema);
+const { Kuliah, Mahasiswa, Makul } = require('./models');
 
 const buatArray = (base = 0, limit = 1) =>
   [...Array(limit - base)].map((x) => base++);
 
-const arr = buatArray(10054, 10100);
+const arr = buatArray(10054, 11000);
 
-Promise.all(
-  arr.map((nim) => {
-    request.get(
-      `http://en.dinus.ac.id/mahasiswa/A11.2017.${nim}`,
-      (error, response, html) => {
-        const $ = cheerio.load(html);
-        const bazz = $('.row')
-          .find('.col-md-5')
-          .html();
-        if (bazz != null) {
-          $('.row')
-            .find('.col-md-5')
-            .find('table')
-            .find('tbody')
-            .each((i, el) => {
-              const tr = $(el)
-                .find('tr')
-                .each((id, foo) => {
-                  const temp = $(foo).find('td');
-                  const label = $(temp[0]).text();
-                  const value = $(temp[2])
-                    .text()
-                    .split(' ');
-                  const ipk = parseFloat(value[0]);
-                  if (label.includes('IPK')) {
-                    const hasil = $(el).find('tr');
-                    const bar = $(hasil).find('td');
-                    const nama = $(bar[2]).text();
-                    console.log({ nim: `A11.2017.${nim}`, nama, ipk });
-                    Mahasiswa.save({ nim: `A11.2017.${nim}`, nama, ipk });
-                  }
-                });
-            });
-        }
-      },
-    );
-  }),
-);
-
-Mahasiswa.find().then((res) => console.log(res));
+arr.map((nim) => {
+  request(
+    `https://en.dinus.ac.id/mahasiswa/A11.2017.${nim}`,
+    async (error, response, html) => {
+      const $ = cheerio.load(html);
+      const check = $('.row')
+        .find('.col-md-5')
+        .html();
+      if (check != null) {
+        const table = $(html).find('table');
+        const biodata = $(table[0])
+          .find('tbody')
+          .find('tr');
+        // console.log($(biodata[3]).html());
+        const kuliah = $(table[1])
+          .find('tbody')
+          .find('tr');
+        const name = $(biodata[0])
+          .find('td')
+          .next()
+          .next()
+          .text();
+        const nim = $(biodata[1])
+          .find('td')
+          .next()
+          .next()
+          .text();
+        const ipk = parseFloat(
+          $(biodata[3])
+            .find('td')
+            .next()
+            .next()
+            .text()
+            .split(' ')[0],
+        );
+        const mhs = Mahasiswa({ nim, name, ipk });
+        await mhs.save();
+        $(kuliah).each(async (id, tr) => {
+          const td = $(tr).find('td');
+          const code = $(td[1]).text();
+          const name = $(td[3]).text();
+          const status = $(td[5]).text();
+          let makul = {};
+          const temp = Makul.find({ name });
+          if (temp.length == undefined) {
+            makul = Makul({ code, name });
+            await makul.save();
+          } else {
+            makul = temp[0];
+          }
+          const kuliah = Kuliah({
+            mahasiswa: mhs._id,
+            makul: makul._id,
+            status,
+          });
+          await kuliah.save();
+          console.log({ nim: `A11.2017.${nim}`, success: true });
+        });
+      }
+    },
+  );
+});
